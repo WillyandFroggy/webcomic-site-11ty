7f4d0e748994ad5a426a882469d7b5836e448b97
WillyandFroggy <willyandfroggy@gmail.com>
2024-07-18 21:34:38 -0400

    init

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+// const Card = require('../src/_includes/components/Card');
+
+module.exports = function (eleventyConfig) {
+  // eleventyConfig.addPassthroughCopy("src/assets/");
+  // eleventyConfig.addPassthroughCopy("src/css/");
+
+  // eleventyConfig.addWatchTarget("src/css/");
+
+  // eleventyConfig.addShortcode("Card", Card);
+
+  // eleventyConfig.addCollection('posts', function(collectionApi) {
+  //   return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
+  // })
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
