9de5af33eca61a92a7a4f14ec5d6dd972fe334e0
WillyandFroggy <willyandfroggy@gmail.com>
2024-11-05 10:55:39 -0500

    removed unused components

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,8 @@
-const ComicViewer = require("./src/_includes/components/ComicViewer");
-const ComicInfo = require("./src/_includes/components/ComicInfo");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addWatchTarget("src/assets/css/");
-
-  eleventyConfig.addShortcode("ComicViewer", ComicViewer);
-  eleventyConfig.addShortcode("ComicInfo", ComicInfo);
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-
-  eleventyConfig.addCollection("comics", function (collection) {
-    return collection.getAll();
-  });
 
   // eleventyConfig.addCollection('posts', function(collectionApi) {
   //   return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
