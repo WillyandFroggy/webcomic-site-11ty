96c4e3c0a6fd40aeaa84ba3df747cb77a8122bfb
WillyandFroggy <willyandfroggy@gmail.com>
2026-01-07 19:23:01 -0500

    Pages done, just need to update links

--- src/_includes/components/ComicCard.js
+++ src/_includes/components/ComicCard.js
@@ -2,19 +2,19 @@ const { html } = require("common-tags");
 
 function ComicCard({
   title,
+  link,
   backgroundColor,
   imgUrl
 }) {
   return html`
-    <div class="comic-card" style="background-color:${backgroundColor}">
+    <div class="comic-card">
       <div class="comic-card__container">
-        <a href="#" class="comic-card__image">
+        <a href="${link}" class="comic-card__image" style="background-color:${backgroundColor}">
           <img
             alt="${title}"
             loading="lazy"
             decoding="async"
-            sizes="(min-width: 400px) 400px, 100vw"
-            style="object-fit:cover;max-width:400px;max-height:350px;aspect-ratio:1.1428571428571428;width:100%"
+            sizes="(min-width: 400px) 428px, 100vw"
             srcset="
               ${imgUrl}&amp;width=400&amp;height=350 400w,
               ${imgUrl}&amp;width=640&amp;height=560 640w,
@@ -23,6 +23,9 @@ function ComicCard({
             "
             src="${imgUrl}&amp;width=400&amp;height=350"
           />
+        </a>
+        <a href="${link}" class="comic-card__title">
+          <h2>${title}</h2>
         </a>
       </div>
     </div>
